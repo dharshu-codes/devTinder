4fe5f76066d2b0d5d98b10ef2b5bbc135c1e83a1
Dharshini <dharshukumar1303@gmail.com>
2026-04-20 20:50:23 +0530

    create a post/signup api function

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 const express = require("express")
-const userauth = require("./middlewares/userauth")
+// const userauth = require("./middlewares/userauth")
 const connectdb = require("./config/database")
+const User = require("./models/user")
 // step : create a server, listen to port, send the response
 
 
 const app = express(); // creates an express js application
+
+app.use(express.json())
+
+app.post("/signup",(req,res)=>{
+    
+    const user= new User(req.body)
+    user.save();
+    res.send("User reated successfully");
+})
 connectdb()
 .then(()=>{
     console.log("Successfully db connected");
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const db= require("mongoose")
 
 const connectdb= async ()=>{
-   await db.connect("url of the db");
+    await db.connect("mongodb://dharshinik1303_db_user:bkr91lj4RXkanC@ac-xqflu44-shard-00-00.vsueggj.mongodb.net:27017,ac-xqflu44-shard-00-01.vsueggj.mongodb.net:27017,ac-xqflu44-shard-00-02.vsueggj.mongodb.net:27017/?ssl=true&replicaSet=atlas-11lnq9-shard-0&authSource=admin&appName=node-js");
 }
 
 module.exports= connectdb;
