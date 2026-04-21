12a68f4e18cf27508a5c764ff9e3966be00a73bb
Dharshini <dharshukumar1303@gmail.com>
2026-04-21 09:26:28 +0530

    create a get and delete/user  api function

--- src/app.js
+++ src/app.js
@@ -9,11 +9,44 @@ const app = express(); // creates an express js application
 
 app.use(express.json())
 
-app.post("/signup",(req,res)=>{
+app.post("/signup",async (req,res)=>{
     
     const user= new User(req.body)
-    user.save();
-    res.send("User reated successfully");
+    try{
+    await user.save();
+    res.send("User created successfully");
+    }
+    catch(err){
+        res.status(400).send("Error something went wrong",err.message)
+    }
+})
+app.get("/user",async (req,res)=>{
+    const useremail= req.body.emailId;
+    try{
+        // use fineOne for getting exactly one  record of the passed property
+    const user= await User.findOne({emailId: useremail});
+    if(user.length ===0){
+        res.status(400).send("User not found")
+    }else{
+        res.send(user)
+    }
+    }
+    catch(err){
+        console.log(err.message)
+        res.status(400).send("Could not find user", err.message)
+    }
+})
+app.delete("/user",async (req,res)=>{
+    const userId= req.body.userId;
+    try{
+        // use fineOne for getting exactly one  record of the passed property
+    const user= await User.findByIdAndDelete(userId);
+    res.send(user)
+    }
+    catch(err){
+        console.log(err.message)
+        res.status(400).send("Could not find user", err.message)
+    }
 })
 connectdb()
 .then(()=>{
